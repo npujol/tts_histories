c84a09fec6052e422ef48385361d0385bab26630
npujol <naivy.luna@gmail.com>
2019-08-01 18:40:12 +0200

    Add main

--- tts_histories.py
+++ tts_histories.py
@@ -1,3 +1,10 @@
+import sys
+import re
+
+RE_SPACES = re.compile(r"\s+")
+RE_NON_ALPHANUMERIC = re.compile(r"\W+")
+
+
 class Story:
     def __init__(self, filename, language="es"):
         self.filename = filename
@@ -8,7 +15,10 @@ class Story:
     def clean_text(self):
         with open(self.filename) as f:
             content = f.readlines()
-        content = " ".join([x.strip() for x in content])
+        content = RE_SPACES.sub(
+            " ", RE_NON_ALPHANUMERIC.sub("", " ".join([x.strip() for x in content]))
+        )
+
         print(content)
         self.text_story = content
 
@@ -18,3 +28,10 @@ class Story:
         tts = gTTS(self.text_story, lang=self.language)
         tts.save("{}.mp3".format(self.filename))
         print("Compleat")
+
+
+if __name__ == "__main__":  # pragma: no coverage
+    if sys.argv[1]:
+        story = Story(sys.argv[1])
+        # story.create_TTS()
+
