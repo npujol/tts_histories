dc671769314b94b25aa9f18f6b9a39579c1b4202
npujol <naivy.luna@gmail.com>
2023-05-07 14:48:33 +0200

    Add tests

---
+++ tests/tts/test_ao3.py
@@ -0,0 +1,66 @@
+from tests.tts.conftest import REAL_AO3_URL
+from tts.ao3 import AO3
+from pathlib import Path
+from tts.serializers import Language
+
+import pytest
+
+
+def test_init():
+    url = "https://archiveofourown.org/works/123456"
+    ao3 = AO3(url)
+    assert ao3.story.url == url
+    assert ao3.story.language == Language.SPANISH
+
+
+def test_write(temp_dir: Path):
+    file_path = temp_dir.joinpath("test.txt")
+    ao3 = AO3("https://archiveofourown.org/works/123456")
+    ao3.write(file_path, "test content")
+    with open(file_path) as f:
+        assert f.read() == "test content"
+
+
+def test_rename(temp_dir: Path):
+    file_path = temp_dir.joinpath("test.txt")
+    file_path.touch()
+    ao3 = AO3("https://archiveofourown.org/works/123456")
+    new_name = "new_file_name"
+    ao3.rename(file_path, new_name)
+    new_path = temp_dir.joinpath(f"{new_name}.txt")
+    assert not file_path.exists()
+    assert new_path.exists()
+    assert ao3.story.text_path == new_path
+
+
+def test_init_creates_file():
+    url = "https://archiveofourown.org/works/123456"
+    ao3 = AO3(url)
+    assert ao3.story.text_path.exists()
+
+
+def test_init_unique_file():
+    url1 = "https://archiveofourown.org/works/123456"
+    url2 = "https://archiveofourown.org/works/654321"
+    ao3_1 = AO3(url1)
+    ao3_2 = AO3(url2)
+    assert ao3_1.story.text_path.name != ao3_2.story.text_path.name
+
+
+@pytest.mark.vcr()
+def test_ao3_save(
+    snapshot,  # type: ignore
+    temp_dir: Path,
+):
+    temp_file = temp_dir / "temp.txt"
+    temp_file.touch()
+
+    ao3 = AO3(
+        url=REAL_AO3_URL,
+        language=Language.SPANISH,
+    )
+    filename = ao3.save()
+    assert filename.is_file()
+    assert snapshot() == temp_file.read_text()
+    assert snapshot() == ao3.story.title
+    assert filename.name.startswith(ao3.story.title)
---
+++ tests/tts/test_wattpad.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from tts.wattpad import Wattpad, Language
+from tests.tts.conftest import REAL_WATTPAD_URL
+import pytest
+
+
+def test_init(
+    wattpad: Wattpad,
+    snapshot,  # type: ignore
+):
+    # Check that the Wattpad object was created successfully
+    assert wattpad.story.url == REAL_WATTPAD_URL
+    assert wattpad.story.language == Language.ENGLISH
+    assert isinstance(wattpad.story.text_path, Path)
+    assert snapshot() == "|".join(
+        sorted(p.model_dump_json() for p in wattpad.story.chapters)
+    )
+
+
+@pytest.mark.vcr()
+def test_save(
+    snapshot,  # type: ignore
+):
+    wattpad = Wattpad(REAL_WATTPAD_URL, language=Language.ENGLISH)
+    # Check that the save method works as expected
+    path = wattpad.save()
+    assert snapshot() == "|".join(
+        sorted(ch.text for ch in wattpad.story.chapters)
+    )
+    assert isinstance(path, Path)
+
+
+@pytest.mark.vcr()
+def test_write(wattpad: Wattpad, temp_dir: Path):
+    # Check that the _write method works as expected
+    file_path = temp_dir / "test.txt"
+    wattpad._write(file_path, "Test content")
+    assert file_path.read_text() == "Test content"
+
+
+@pytest.mark.vcr()
+def test_rename(wattpad: Wattpad, temp_dir: Path):
+    # Check that the _rename method works as expected
+    old_path = temp_dir / "old.txt"
+    old_path.write_text("Old content")
+    new_name = "new"
+    wattpad._rename(old_path, new_name)
+    new_path = temp_dir / f"{new_name}.txt"
+    assert not old_path.exists()
+    assert new_path.exists()
+    assert new_path.read_text() == "Old content"
